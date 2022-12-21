47c7637378e642ff25fc2f6ef105e6ffb37a5143
Belivups <belivup@gmail.com>
2022-12-21 23:35:26 +0600

    api

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,3 +1,38 @@
+import {useState} from "react";
+import ShowImages from "./ShowImages";
+
+function SearchBox({fetchImage}) {
+
+    const [text, setText] = useState('')
+
+    function handleSubmit(){
+        fetchImage(text)
+    }
+
+    return (
+        <>
+            <div className={'header'}>
+                <div className="logo">
+                    <h3><a href="/">React Image Search</a></h3>
+                </div>
+                <div className="search">
+                    <form action="" onSubmit={(e) => {
+                        e.preventDefault()
+                        handleSubmit()
+                    }} >
+                        <input type="text" value={text} onChange={ (e) => setText(e.target.value)} placeholder={'Enter Keywords'} className={'field'}/>
+                    </form>
+                </div>
+                <div className="btn">
+                    <button onClick={handleSubmit}>Search</button>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default SearchBox;
 
 
 function SearchBox(props) {
---
+++ src/components/ShowImages.js
@@ -0,0 +1,52 @@
+import image from '../assets/Image.png'
+import {useState} from "react";
+import SearchBox from "./SearchBox";
+
+export default function ShowImages() {
+
+    const [imageList, setImageList] = useState([])
+    const [text, setText] = useState('')
+
+    async function fetchImage(text){
+        setText(text)
+
+        await fetch(`https://api.unsplash.com/search/photos?&query=${text}`,{
+            headers:{
+                Authorization: 'Client-ID orXhS0zmJLZqnl9dzm_S9TBfCz5icIIc0FNRXZxXXUc'
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => setImageList(data.results))
+            .catch( (err)=> console.log(err))
+    }
+
+    return (
+
+        <>
+
+        <div className="main">
+            <div className="containers">
+                <SearchBox fetchImage={fetchImage}/>
+            </div>
+        </div>
+
+        <div className={'showImageContainer'}>
+           <div className="breadcrumbs">
+               <h3>{text == '' ? `Search For Images` : `${text.toLocaleUpperCase()} Images listed here` }</h3>
+           </div>
+
+            {imageList.map(e => {
+
+                return(
+                    <div className="images" key={e.id}>
+                        <img src={e.urls.regular}/>
+                        <h4 className={'title'}>{e.alt_description}</h4>
+                    </div>
+                )
+            })}
+
+        </div>
+
+        </>
+    )
+}
